ca1596648040f01d04aad3b0586d6ae6f30172f6
nikki30 <nikita.siva@gmail.com>
2016-05-12 23:15:30 -0700

    Image
    Adding an image to the screen.
    On the click of the button, the image is replaced by another

--- mainActivity.java
+++ mainActivity.java
@@ -1,4 +1,4 @@
-package com.nikki.myfirstandroidapp;
+package com.nikki.displaytouser;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,43 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
-    public void clickFunction(View view)
-    {
-        Toast.makeText(getApplicationContext(), "Hi Nikki", Toast.LENGTH_LONG).show();
-        EditText mytext = (EditText) findViewById(R.id.textField);
-        Log.i("Text field",mytext.getText().toString());
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);package com.nikki.displaytouser;
-
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.Toast;
-import android.content.Context;
+import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     public void clickMe(View view)
     {
-       EditText myTextField = (EditText) findViewById(R.id.editText2);
-        Log.i("Info",myTextField.getText().toString());
-        Toast.makeText(getApplicationContext(),"Hi "+myTextField.getText().toString(), Toast.LENGTH_LONG).show();
-
+        ImageView picture1 = (ImageView) findViewById(R.id.pic1);
+        picture1.setImageResource(R.drawable.pic2);
     }
 
 
@@ -87,7 +57,29 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+package com.nikki.displaytouser;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+public class MainActivity extends AppCompatActivity {
+
+    public void clickMe(View view)
+    {
+        ImageView picture1 = (ImageView) findViewById(R.id.pic1);
+        picture1.setImageResource(R.drawable.pic2);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
