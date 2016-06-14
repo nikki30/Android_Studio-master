0c9894009887e46d5303c1142e49cc696f9a7836
nikki30 <nikita.siva@gmail.com>
2016-06-14 16:38:56 -0700

    Basics Of using JSONObject and JSONArray
    Effective way to get information(in this case, weather info) from an
    online API source (in this case, Open Weather Map).
    JSON is effective as it can use the information from API's directly and
    also, the app won't break easily as API's don't change their HTML as
    often as websites.

---
+++ jsonProcess.java
@@ -0,0 +1,115 @@
+package com.nikki.processingjson;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadWebContent extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = " ";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String weather = jsonObject.getString("weather");  //gets only the part with weather : here there is only one "{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}" but sometimes there are two, in which case, we need to use an array as follows
+                JSONArray arr = new JSONArray(weather);
+                for(int i=0;i<arr.length();i++){
+                    JSONObject jsonPart = arr.getJSONObject(i);  //treats {"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"} as first object and the other one as second
+                    Log.i("Main",jsonPart.getString("main"));
+                    Log.i("Description",jsonPart.getString("description"));
+                }
+               /// Log.i("WEBSITE CONTENT:", weather);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DownloadWebContent task  = new DownloadWebContent();
+        task.execute("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=33b49c4258043c3bb9f6451d2edd0a32");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
